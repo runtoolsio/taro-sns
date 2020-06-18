5cb1cf82fb4e94a4734ec5323245f1591738efb8
stan svec <stan.x.svec@gmail.com>
2020-06-18 12:21:15 +0100

    more warning info

--- taro_sns/notification.py
+++ taro_sns/notification.py
@@ -34,6 +34,8 @@ def _create_job_section(job: JobInfo, *, always_exec_time: bool):
     s += "\nState changed: " + str(job.lifecycle.last_changed())
     if job.state.is_terminal() or always_exec_time:
         s += "\nExecution Time: " + taro.format_timedelta(job.lifecycle.execution_time())
+    if job.warnings:
+        s += "\nWarnings: " + ",".join((w.id for w in job.warnings))
     return s
 
 
@@ -78,6 +80,8 @@ class SnsNotification(ExecutionStateObserver, JobWarningObserver):
         subject = "Job {} changed state from {} to {}".format(job.job_id, prev_state.name, cur_state.name)
         sections = [_create_job_section(job, always_exec_time=False), _create_hostinfo_section(self.hostinfo)]
 
+        if job.warnings:
+            subject += " with warnings!"
         if cur_state.is_failure():
             sections.append(_create_error_section(job, job.exec_error))
 
